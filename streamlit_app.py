2ae38b8059812c948e5af29a409fd72c288537ef
sohansourab <sohan.doc2705@gmail.com>
2026-07-09 14:30:30 +0000

    rollback

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 import pandas as pd
 import plotly.express as px
-import docustruct.app_runner as st
+import docustruct.streamlit as st
 
 sys.path.append(str(Path(__file__).resolve().parent))
 
